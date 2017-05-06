41b93b980b4888b154c87803ae08c7f5b30fded5
andreiQQQ <dezinhibat@gmail.com>
2017-05-06 14:55:20 +0300

    Add files via upload

---
+++ CardZone.cpp
@@ -0,0 +1,84 @@
+#include "CardZone.h"
+#include <iostream>
+
+using namespace Theta;
+
+extern GameObject* moving;
+
+CardZone::CardZone(float width, float height, float wp, float hp) {
+
+	this->setSize(sf::Vector2f(width, height));
+	this->setPosition(sf::Vector2f(wp, hp));
+	nr_cards = 0;
+	float card_size_X = (width - 20 * (NR_MAX_CARDS + 1)) / NR_MAX_CARDS;
+	float card_size_Y = height - 20 * 2;
+	for (int i = 0; i < NR_MAX_CARDS; i++) {
+
+		PH_CARDS[i].SetCardRect(card_size_X, card_size_Y, wp + 20 * (i + 1) + card_size_X * i, hp + 20);
+		
+		/** Must be changed !! **/
+		PH_CARDS[i].setTextPosition(wp + 20 * (i + 1) + card_size_X * i, hp + 20);
+	}
+}
+
+void CardZone::draw(sf::RenderWindow& window) {
+
+	for (unsigned int i = 0; i < nr_cards; i++)
+	{
+		if (&PH_CARDS[i] != moving)
+			PH_CARDS[i].draw(window);
+	}
+}
+
+void CardZone::add_card(const std::string& where_to_find) {
+
+	if (nr_cards < NR_MAX_CARDS) {
+
+		nr_cards++;
+		PH_CARDS[nr_cards - 1].SetCardSprite(where_to_find);
+	}
+}
+
+void CardZone::erase_card(unsigned int indx) {
+
+	if (indx >= 0 && indx < nr_cards) {
+
+		for (unsigned int i = indx; i < nr_cards; i++)
+		{
+			PH_CARDS[i].copy_content(PH_CARDS[i + 1]);
+		}
+		nr_cards--;
+	}
+}
+
+void CardZone::copy_card(CardZone* cz_ptr, unsigned int indx) {
+
+	if (cz_ptr != nullptr && indx >= 0 && indx < cz_ptr->nr_cards && nr_cards < NR_MAX_CARDS) {
+
+		nr_cards++;
+		PH_CARDS[nr_cards - 1].copy_content(cz_ptr->PH_CARDS[indx]);
+	}
+}
+
+bool CardZone::isFull() {
+
+	return (nr_cards == NR_MAX_CARDS);
+}
+
+void CardZone::setRectColor(const sf::Color& color) {
+		
+	this->setFillColor(color);
+}
+
+void CardZone::setRectTexture(const std::string& where_to_find) {
+
+	if (where_to_find != "\0") {
+
+		if (!cardzone_texture.loadFromFile(where_to_find)) {
+
+			std::cerr << "Error: Could not open program's data !\n";
+			exit(1);
+		}
+		this->setTexture(&(this->cardzone_texture));
+	}
+}
---
+++ Table.cpp
@@ -0,0 +1,44 @@
+#include "Table.h"
+#include <iostream>
+
+using namespace Theta;
+
+extern GameObject* moving;
+
+Table::Table(float width, float height) :
+
+player_hand(width, 0.33f * height, 0.0f, 0.66f * height),
+player_table(width, 0.33f * height, 0.0f, 0.33f * height),
+enemy_table(width, 0.33f * height, 0.0f, 0.0f) {
+
+	this->setSize(sf::Vector2f(width, height));
+	this->setRectTexture("background.png");
+}
+
+void Table::draw(sf::RenderWindow& window) {
+
+	window.draw((sf::RectangleShape) (*this));
+	//window.draw((sf::RectangleShape)player_hand);
+	//window.draw((sf::RectangleShape)player_table);
+	//window.draw((sf::RectangleShape)enemy_table);
+	player_hand.draw(window);
+	player_table.draw(window);
+	enemy_table.draw(window);
+
+	CardSprite* moving_card = static_cast <CardSprite*> (moving);
+	if (moving_card != nullptr)
+		moving_card->draw(window);
+}
+
+void Table::setRectTexture(const std::string& where_to_find) {
+
+	if (where_to_find != "\0") {
+
+		if (!table_texture.loadFromFile(where_to_find)) {
+
+			std::cerr << "Error: Could not open program's data !\n";
+			exit(1);
+		}
+		this->setTexture(&(this->table_texture));
+	}
+}
